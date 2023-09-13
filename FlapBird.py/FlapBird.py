57be6f04daeb0edad87e555c9432e26e3fed9886
Frederico Ferrarezi <eng.producaoffdc@gmail.com>
2023-09-12 21:47:15 -0300

    Terminando o passaro

--- FlapBird.py/FlapBird.py
+++ FlapBird.py/FlapBird.py
@@ -5,17 +5,24 @@ import random
 TELA_LARGURA = 500
 TELA_ALTURA = 800
 
-IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'pip.png')))
-IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'base.png')))
-IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'bg.png')))
+IMAGEM_CANO = pygame.transform.scale2x(
+    pygame.image.load(os.path.join("img", "pip.png"))
+)
+IMAGEM_CHAO = pygame.transform.scale2x(
+    pygame.image.load(os.path.join("img", "base.png"))
+)
+IMAGEM_BACKGROUND = pygame.transform.scale2x(
+    pygame.image.load(os.path.join("img", "bg.png"))
+)
 IMAGEM_PASSARO = [
-    pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'bird1.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'bird2.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'bird3.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join("img", "bird1.png"))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join("img", "bird2.png"))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join("img", "bird3.png"))),
 ]
 
 pygame.font.init()
-FONTE_PONTOS = pygame.font.SysFont('arial', 50)
+FONTE_PONTOS = pygame.font.SysFont("arial", 50)
+
 
 class Passaro:
     img = IMAGEM_PASSARO
@@ -32,8 +39,8 @@ class Passaro:
         self.altura = self.y
         self.tempo = 0
         self.contagem_imagem = 0
-        self.imagem = IMG[0]
-    
+        self.imagem = self.IMGS[0]
+
     def pular(self):
         self.velocidade = -10.5
         self.tempo = 0
@@ -48,7 +55,7 @@ class Passaro:
             deslocamento = 16
         elif deslocamento < 0:
             deslocamento -= 2
-        
+
         self.y += deslocamento
 
         # Angulo do passaro
@@ -58,12 +65,40 @@ class Passaro:
         else:
             if self.angulo > -90:
                 self.angulo -= self.VELOCIDADE_ROTACAO
-    
-    def desenhar(self):
+
+    def desenhar(self, tela):
         # definir qual imagem do passaro vai usar
+        self.contagem_imagem += 1
+
+        if self.contagem_imagem < self.TEMPO_ANIMACAO:
+            self.imagem = self.IMGS[0]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO * 2:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO * 3:
+            self.imagem = self.IMGS[2]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO * 4:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO * 4 + 1:
+            self.imagem = self.IMGS[0]
+            self.contagem_imagem = 0
+        # se o passaro tiver caindo eu nao vou bater asa
+        if self.angulo <= -80:
+            self.imagem = self.IMGS[1]
+            self.contagem_imagem = self.TEMPO_ANIMACAO * 2
+
+        # desenhar a imagem
+        imagem_rotacionada = pygame.tranform.rotate(self.imagem, self.angulo)
+        pos_centro_imagem = self.imagem.get_rect(topleft=(self.x, self.y)).center
+        retangulo = imagem_rotacionada.get_rect(center=pos_centro_imagem)
+        tela.blit(imagem_rotacionada, retangulo.topleft)
+
+    def get_mask(self):
+        pygame.mask.from_surface(self.imagem)
+
 
 class Cano:
     pass
+
 
 class Chao:
     pass
